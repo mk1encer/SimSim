e00ad1cb45d479ce9cb2415b6a78da913a725526
mk1encer <mksur1er@gmail.com>
2022-01-12 08:09:52 +0900

    채팅 전송까지 완료

--- src/components/ChatRoom.js
+++ src/components/ChatRoom.js
@@ -1,6 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import { auth, db } from "../Firebase";
+import { ref, set, onValue, push } from "firebase/database";
 
 const ChatRoom = (props) => {
+  const [msg, setMsg] = useState("");
+  const [roomid, setRoomid] = useState("");
+
+  const handleOnChange = (e) => {
+    setMsg(e.target.value);
+  };
+
+  function sendChat(data) {
+    const temp = ref(db, "/chatRooms/");
+    const roomids = [];
+    onValue(temp, (snapshot) => {
+      const rooms = snapshot.val();
+      for (let id in rooms) {
+        roomids.push({ ...rooms[id] });
+      }
+    });
+
+    for (let i in roomids) {
+      if (roomids[i].name === props.name) {
+        const temp = roomids[i].id;
+        setRoomid(temp);
+      }
+    }
+
+    if (roomid != "") {
+      return push(ref(db, "/chatRooms/" + roomid + "/message"), {
+        message: data.message,
+        timestamp: data.timestamp,
+        uid: data.uid,
+      });
+    }
+  }
+
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+    console.log(msg);
+    try {
+      await sendChat({
+        message: msg,
+        timestamp: Date.now(),
+        uid: auth.currentUser.uid,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div
       style={{
@@ -13,27 +62,53 @@ const ChatRoom = (props) => {
     >
       <div
         style={{
-          height: "16.8rem",
+          height: "15.5rem",
         }}
       >
-        이 채팅은 ({props.name})채팅방의 채팅내용입니다.
-      </div>
-      <div style={{ flexBasis: "1rem", display: "flex" }}>
-        <input
-          style={{ flex: "1" }}
-          type="text"
-          size="70"
-          placeholder="채팅을 입력하세요"
-        ></input>
-        <button
+        여기는 ({props.name})채팅방의 채팅내용입니다.
+        <li
           style={{
-            backgroundColor: "lightgray",
-            marginTop: "0",
-            width: "8rem",
+            listStyle: "none",
+            textAlign: "left",
+            backgroundColor: "lightgreen",
           }}
         >
-          send
-        </button>
+          hi! &nbsp; <span>05:54</span>
+        </li>
+        <li
+          style={{
+            listStyle: "none",
+            textAlign: "right",
+            backgroundColor: "lightsteelblue",
+          }}
+        >
+          <span>05:56</span> &nbsp; hello
+        </li>
+      </div>
+      <div>
+        <form
+          style={{ flexBasis: "1rem", display: "flex" }}
+          onSubmit={handleOnSubmit}
+        >
+          <input
+            style={{ flex: "1" }}
+            type="text"
+            size="70"
+            placeholder="채팅을 입력하세요"
+            value={msg}
+            onChange={handleOnChange}
+          />
+          <button
+            type="submit"
+            style={{
+              backgroundColor: "lightgray",
+              marginTop: "0",
+              width: "6rem",
+            }}
+          >
+            send
+          </button>
+        </form>
       </div>
     </div>
   );
---
+++ src/components/users.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Me = () =>{
+    return(
+        
+    )
+}
+
+const Other = () =>{
+
+}
+
+export Me;
+export Other;
